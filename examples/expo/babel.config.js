f51bc0a6dbd98b95a84f6351b4d082446117c547
Thet Aung <thetaung.dev@gmail.com>
2025-07-11 13:40:09 +0300

    chore: upgraded unistyles and expo deps

--- examples/expo/babel.config.js
+++ examples/expo/babel.config.js
@@ -1,22 +1,44 @@
 module.exports = function (api) {
     api.cache(true);
-    return {
-        // for bare React Native
-        // presets: ['module:@react-native/babel-preset'],
+    const unistylesPluginOptions = {
+        root: 'src',
+        autoProcessPaths: [
+            // this is not repetition, this is for when linking the molecules
+            'packages/molecules',
+            // this is for when molecules is installed normally
+            '@bambooapp/bamboo-molecules',
+            'packages/ui/bamboo-table',
+            'packages/ui/shared',
+            'packages/ui/chat-module',
+            'packages/ui/table',
+            'packages/ui/core-fields',
+            'packages/ui/dashboard',
+            'packages/ui/onboarding',
+            'packages/ui/lightbox',
+        ],
+    };
 
+    return {
         // or for Expo
-        presets: ['babel-preset-expo'],
+        presets: [
+            [
+                'babel-preset-expo',
+                {
+                    unstable_transformImportMeta: true,
+                },
+            ],
+        ],
 
         // other config
         plugins: [
-            // other plugins
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            ['@babel/plugin-transform-class-properties', { loose: true }],
             [
-                'react-native-unistyles/plugin',
-                {
-                    autoProcessRoot: 'src',
-                    autoProcessPaths: ['packages/molecules'],
-                },
+                '@babel/plugin-transform-runtime',
+                { version: require('@babel/runtime/package.json').version, useESModules: false },
             ],
+            '@babel/plugin-transform-class-static-block',
+            ['react-native-unistyles/plugin', unistylesPluginOptions],
         ],
     };
 };
