a3d14324dd18ecf02516e592ce0655e68c1d5f30
Thet Aung <thetaung.dev@gmail.com>
2025-11-27 00:38:27 +0300

    chore: added a workaround for loading font with prefix

--- docs/src/theme/Root.js
+++ docs/src/theme/Root.js
@@ -3,6 +3,13 @@ import '../unistyles';
 import React from 'react';
 import { PortalProvider } from 'react-native-molecules/components/Portal';
 
+import FontLoader from './FontLoader';
+
 export default function Root({ children }) {
-    return <PortalProvider>{children}</PortalProvider>;
+    return (
+        <PortalProvider>
+            <FontLoader />
+            {children}
+        </PortalProvider>
+    );
 }
